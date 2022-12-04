7b4337b55626552cadd1ab039374b36673687ff8
BerkkanB <butun18@itu.edu.tr>
2022-12-04 20:09:04 +0300

    GraduationProject initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Sidebar from './components/Sidebar';
+import MatchingTable from './components/MatchingTable';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid h-100">
+        <div className="row h-100">
+          <div className="col-2 p-0">
+            <Sidebar />
+          </div>
+          <div className="col-10 p-0">
+            <MatchingTable />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/MatchingTable.js
@@ -0,0 +1,5 @@
+export default function MatchingTable(){
+    return(
+        <div className="container-fluid"> </div>
+    )
+}
