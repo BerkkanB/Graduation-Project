73bb8a4fbd52bf16a50aaf005405ba1250ef45c7
berkkanB <butun18@itu.edu.tr>
2023-01-07 16:08:51 +0300

    matching-table with beautiful dnd impl

--- src/components/MatchingTable.js
+++ src/components/MatchingTable.js
@@ -1,5 +1,127 @@
-export default function MatchingTable(){
-    return(
-        <div className="container-fluid"> </div>
-    )
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import TeacherName from "./TeacherName";
+
+const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+
+/**
+ * Moves an item from one list to another list.
+ */
+const move = (source, destination, droppableSource, droppableDestination) => {
+    const sourceClone = Array.from(source);
+    const destClone = Array.from(destination);
+    const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+    destClone.splice(droppableDestination.index, 0, removed);
+
+    const result = {};
+    result[droppableSource.droppableId] = sourceClone;
+    result[droppableDestination.droppableId] = destClone;
+
+    return result;
+};
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+
+    // change background colour if dragging
+    background: isDragging ? "white" : "white",
+
+    height: "55px",
+    borderRadius: "8px",
+    boxShadow: "0px 3px 4px 0px rgba(0,0,0,0.25)",
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+});
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? "lightgrey" : "white",
+    height: "100vh",
+});
+
+export default function MatchingTable({ data, teachers }) {
+    const [state, setState] = useState(data);
+
+    function onDragEnd(result) {
+        const { source, destination } = result;
+
+        // dropped outside the list
+        if (!destination) {
+            return;
+        }
+        const sInd = +source.droppableId;
+        const dInd = +destination.droppableId;
+
+        if (sInd === dInd) {
+            const items = reorder(state[sInd], source.index, destination.index);
+            const newState = [...state];
+            newState[sInd] = items;
+            setState(newState);
+        } else {
+            const result = move(state[sInd], state[dInd], source, destination);
+            const newState = [...state];
+            newState[sInd] = result[sInd];
+            newState[dInd] = result[dInd];
+
+            setState(newState.filter(group => group.length));
+        }
+    }
+
+    return (
+        <div>
+            <div style={{ display: "flex" }}>
+                <DragDropContext onDragEnd={onDragEnd}>
+                    {state.map((el, ind) => (
+                        <Droppable key={ind} droppableId={`${ind}`}>
+                            {(provided, snapshot) => (
+                                <div
+                                    className="m-0 p-0"
+                                    ref={provided.innerRef}
+                                    style={getListStyle(snapshot.isDraggingOver)}
+                                    {...provided.droppableProps}
+                                >
+                                    <TeacherName name={teachers[ind]} />
+                                    {el.map((item, index) => (
+                                        <Draggable
+                                            key={item.id}
+                                            draggableId={item.id}
+                                            index={index}
+                                        >
+                                            {(provided, snapshot) => (
+                                                <div
+                                                    className="mb-2 bg-light d-flex text-dark align-items-center justify-content-center ms-2 me-2 p-2"
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                    style={getItemStyle(
+                                                        snapshot.isDragging,
+                                                        provided.draggableProps.style
+                                                    )}
+                                                >
+                                                    <div >
+                                                        <h5>{item.name}</h5>
+                                                    </div>
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                    ))}
+                </DragDropContext>
+                <button className="btn btn-primary" style={{ position: "sticky", top: "90vh", right: "30px", height: "60px", width: "60px", borderRadius: "30px", boxShadow: "0px 3px 4px 0px rgba(0,0,0,0.25)" }} onClick={() => console.log(state)}>Save</button>
+            </div>
+        </div>
+    );
 }
+
---
+++ src/components/TeacherName.js
@@ -0,0 +1,7 @@
+export default function TeacherName({name}){
+    return (
+        <div className="mb-1 bg-dark d-flex text-white align-items-center justify-content-center" style={{height:"70px"}}>
+            <h4>{name}</h4>
+        </div>
+    )
+}
